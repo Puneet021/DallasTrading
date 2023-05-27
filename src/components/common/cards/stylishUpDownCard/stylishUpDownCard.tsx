3b57b79ab680d186d5cb93b379a9837a1a1d603b
Puneet-DIT <puneetsharma2@deloitte.com>
2023-05-27 20:53:18 +0530

    animation added in reviews and cards

--- src/components/common/cards/stylishUpDownCard/stylishUpDownCard.tsx
+++ src/components/common/cards/stylishUpDownCard/stylishUpDownCard.tsx
@@ -4,15 +4,26 @@ import {
   IStylishUpDownCardProps,
   IStylishUpDownCardStates,
 } from "./stylishUpDownCard.constants";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 class StylishUpDownCard extends Component<
   IStylishUpDownCardProps,
   IStylishUpDownCardStates
 > {
+  componentDidMount(): void {
+    Aos.init();
+  }
   render(): ReactNode {
     const { altText, image } = this.props;
     return (
-      <div className={styles.card}>
+      <div
+        className={styles.card}
+        data-aos="zoom-in-up"
+        data-aos-delay="20"
+        data-aos-offset="100"
+        data-aos-duration="1000"
+      >
         <div className={styles.left}>
           <img className={styles.img} src={image} alt={altText} />
         </div>
--- src/modules/product-details/reviews/customerReviews/customerReviews.tsx
+++ src/modules/product-details/reviews/customerReviews/customerReviews.tsx
@@ -8,17 +8,29 @@ import { connect } from "react-redux";
 import { IStore } from "../../../../utils/models/store.model";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import { getProductDetailReviews } from "../../../../store/product-detail/productDetailActions";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 class CustomerReviews extends Component<
   ICustomerReviewsProps,
   ICustomerReviewsStates
 > {
+  componentDidMount(): void {
+    Aos.init();
+  }
   render(): ReactNode {
     return (
       <div className={styles.customerReviewsContainer}>
         <h2 className={styles.heading2}>Customer Reviews</h2>
         {this.props.customerReviewsData.map((item, ind) => (
-          <div key={ind} className={styles.review}>
+          <div
+            key={ind}
+            className={styles.review}
+            data-aos="fade-up"
+            data-aos-delay="20"
+            data-aos-offset="70"
+            data-aos-duration="1000"
+          >
             <h2 className={styles.reviewer}>{item.customerName}</h2>
             <div className={styles.rating}>
               <h4 className={styles.email}>{item.emailId}</h4>
