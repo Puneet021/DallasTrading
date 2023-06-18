a3d223d888e3e951fc9462d9876e37434dfa92a8
Puneet-DIT <puneetsharma2@deloitte.com>
2023-06-18 12:47:04 +0530

    products added

--- src/store/products-category/productsCategoryImages/productsCategoryImages.ts
+++ src/store/products-category/productsCategoryImages/productsCategoryImages.ts
@@ -1,11 +1,39 @@
-import Industrial_Fans from "./category1.png";
-import PVC_Conduits from "./category2.png";
-import Cables from "./category3.png";
-import Cable_Ties from "./category4.png";
+import Industrial_Fans from "./industrial_fan.png";
+import PVC_Conduits from "./pvc_conduits.png";
+import Cables from "./cables.png";
+import Cable_Ties from "./cable_ties.png";
+import Wires from "./wire.png";
+import Jointing_Kit from "./cable-termination-kit.jpg";
+import Industrial_Plugs from "./industrial_plugs_and_sockets.jpg";
+import High_Voltage_Connectors from "./high_voltage_connectors.png";
+import Industrial_Cable_Glands from "./industrial_cable_glands.jpg";
+import Industrial_Cable_Lugs_And_Connectors from "./cable-lugs-connectors.jpg";
+import Cable_Management_System from "./cable-management-system.jpg";
+import FlexibleRigid_Conduit_And_Accessories from "./flexible-conduit-accessories.jpg";
+import Switchgear_Accessories from "./hanger-switchgear.jpg";
+import Switchgear_Assembly from "./Switchgear-Assembly.png";
+import Wiring_Devices from "./wire-accessories.jpg";
+import GI_Boxes from "./switch-socket-box.jpg";
+import Scotch_Tapes_And_Mastics from "./scotch-33-tape.jpg";
+import Laser_Marking_Machine from "./laser-printer-machine.png";
 
 export const productsCategoryImages: { [id: string]: string } = {
   Industrial_Fans: Industrial_Fans,
   PVC_Conduits: PVC_Conduits,
   Cables: Cables,
   Cable_Ties: Cable_Ties,
+  Wires: Wires,
+  Jointing_Kit: Jointing_Kit,
+  Industrial_Plugs: Industrial_Plugs,
+  High_Voltage_Connectors: High_Voltage_Connectors,
+  Industrial_Cable_Glands: Industrial_Cable_Glands,
+  Industrial_Cable_Lugs_And_Connectors: Industrial_Cable_Lugs_And_Connectors,
+  Cable_Management_System: Cable_Management_System,
+  FlexibleRigid_Conduit_And_Accessories: FlexibleRigid_Conduit_And_Accessories,
+  Switchgear_Accessories: Switchgear_Accessories,
+  Switchgear_Assembly: Switchgear_Assembly,
+  Wiring_Devices: Wiring_Devices,
+  GI_Boxes: GI_Boxes,
+  Scotch_Tapes_And_Mastics: Scotch_Tapes_And_Mastics,
+  Laser_Marking_Machine: Laser_Marking_Machine,
 };
